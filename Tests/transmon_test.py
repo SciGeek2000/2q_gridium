6da2bc54cc134123d06fdc3c9062e5e0604adf99
Thomas Ersevim <ersevimt@berkeley.edu>
2026-02-05 15:32:04 -0800

    Updating a Transmon test a little bit to do convergence tests

--- Tests/transmon_test.py
+++ Tests/transmon_test.py
@@ -6,18 +6,60 @@
 import pytest
 import scqubits #https://scqubits.readthedocs.io/en/latest/installation.html#arm64-compatibility
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+
 
 sys.path.append('/Users/thomasersevim/QNL/2q_gridium')
-import Circuit_Objs.qchard_transmon as transmon
+from Circuit_Objs.qchard_transmon import *
 
 def test_eigenvals():
-    # Ensures the eigenvalues of the qchard transmon are within 2% of known scqubits case!
+    error_threshold = 0.02
     scTransmon = scqubits.Transmon(EC=1, EJ=50, ng=0, ncut=15)
     zeroed_scTransmon_vals = (scTransmon.eigenvals() - scTransmon.eigenvals()[0])[1:]
-    
-    qchard_transmon = transmon.Transmon(E_C=1, E_J=50, n_g=0, nlev=6, nlev_lc=25, units='GHz')
+    qchard_transmon = Transmon(E_C=1, E_J=50, n_g=0, nlev=6, nlev_lc=25, units='GHz')
     zeroed_qchardTransmon_vals = qchard_transmon.transition_energies()[1:]
-    
     remainder = (1 - (zeroed_qchardTransmon_vals/zeroed_scTransmon_vals))
     max_off = max(abs(remainder))
-    assert max_off < 0.02
+    assert max_off < error_threshold
+    
+@pytest.mark.parametrize('tested_transmon', [])
+def test_convergence(tested_transmon, plotting=False):
+    '''
+    A test for the convergence of the gridium spectra so that as a function of the nlev_lc, the
+    change is negligible (i.e. the asymptote gives within less than 1 percent of the chosen
+    nlev_lc cutoff value.
+    
+    :param tested_gridium: The gridium used for the convergence test
+    '''
+
+    copied_transmon = copy.deepcopy(tested_transmon)
+    error_threshold = 1e-4
+    nlev = copied_transmon.nlev
+
+    if plotting == False: 
+        default_lc_transitions = copied_transmon.transition_energies(nlev=nlev)[1:]
+        copied_transmon.nlev_lc = copied_transmon.nlev_lc*2
+        twice_lc_transitions = copied_transmon.transition_energies(nlev=nlev)[1:]
+        pct_error = (default_lc_transitions-twice_lc_transitions)/np.max(np.abs(twice_lc_transitions))
+        worst_error = np.max(np.abs(pct_error)) 
+        assert worst_error < error_threshold # For testing, the assertion is that the precent error, scaled to the largest transition (to get around zeros) should be < error_threshold
+
+    if plotting == True: # To visually ensure that the convergence is good and stable.
+        plotting_points = 50
+        nlevs_lc = np.linspace(10,copied_transmon.nlev_lc*2, plotting_points, dtype=int)
+        convergence_array = np.empty((plotting_points, nlev))
+        for i, nlev_lc in enumerate(nlevs_lc):
+            copied_transmon.nlev_lc = nlev_lc
+            convergence_array[i,:] = copied_transmon.transition_energies(nlev=nlev)
+        for i in range(nlev):
+            plt.plot(nlevs_lc, convergence_array[:,i], label=r'0$\rightarrow$' + str(i+1))
+        plt.xlabel('H.O. Basis Cutoff Dimentionality')
+        plt.ylabel(r'Transition Energy ({}/h)'.format(tested_transmon.units))
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+        return
+
+test_convergence(Transmon(50,1,0,5,100), plotting=True)
